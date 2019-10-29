41305db16e2530a2a564ca7944e5cc603f581cc7
Dmitry Galakhov <d.galakhov@gmail.com>
2019-10-29 22:46:48 +0300

    Фикс картинок

--- src/components/steps/HouseChoose.vue
+++ src/components/steps/HouseChoose.vue
@@ -6,21 +6,21 @@
                 <div class="col-lg-4  col-md-8  offset-md-2  offset-lg-0">
                     <card @click="choosePath('flat')" title="В квартиру" 
                     desc="Решения для многоквартирных домов." actiontext="Начать">
-                        <img src="../../assets/flat.png" srcset="../../assets/flat@2x.png 2x"/>
+                        <img src="../../assets/flat.png" srcset="../../assets/flat.png 1x, ../../assets/flat@2x.png 2x"/>
                     </card>
                 </div>
 
                 <div class="col-lg-4  col-md-8  offset-md-2  offset-lg-0">
                     <card @click="choosePath('house')" title="В частный дом"
                     desc="Решения для частных домов с постоянным отоплением." actiontext="Начать">
-                        <img src="../../assets/house.png" srcset="../../assets/house@2x.png 2x"/>
+                        <img src="../../assets/house.png" srcset="../../assets/house.png 1x, ../../assets/house@2x.png 2x"/>
                     </card>
                 </div>
 
                 <div class="col-lg-4  col-md-8  offset-md-2  offset-lg-0">
                     <card @click="choosePath('dacha')" title="На дачу" 
                     desc="Решения для загородного дома с сезонным отоплением." actiontext="Начать">
-                        <img src="../../assets/dacha.png" srcset="../../assets/dacha@2x.png 2x"/>
+                        <img src="../../assets/dacha.png" srcset="../../assets/dacha.png 1x, ../../assets/dacha@2x.png 2x"/>
                     </card>
                 </div>
             </div>
